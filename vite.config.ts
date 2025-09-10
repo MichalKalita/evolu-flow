088a3778994bee13a16c0c728f225563dc14a066
Michal Kalita <kalita.michal@gmail.com>
2025-09-10 02:19:05 +0200

    add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
       injectRegister: false,
